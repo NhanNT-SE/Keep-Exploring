a03d56cbdbf17cc8a5633c9a00b40e0537399db2
Trong Nhan <trongnhannguyen94@gmail.com>
2021-02-04 23:21:12 +0700

    add buildspec.yml file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Keep Exploring Web Admin
-        </p>
+        <p>Keep Exploring Web Admin</p>
+        <p>Server web: https://web-app.duuk7ot2ayt8v.amplifyapp.com/</p>
+        <p>Server backend: https://web-app.duuk7ot2ayt8v.amplifyapp.com/</p>
+        <p>Server db: https://web-app.duuk7ot2ayt8v.amplifyapp.com/</p>
         <a
           className="App-link"
           href="https://reactjs.org"
