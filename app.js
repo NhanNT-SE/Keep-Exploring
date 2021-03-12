8876cdcf2eb719f4ca96bbbe5ac323c6db6626e3
Trong Nhan <trongnhannguyen94@gmail.com>
2021-03-12 20:48:59 +0700

    edit

--- app.js
+++ app.js
@@ -11,10 +11,8 @@ const addressRouter = require('./src/Route/AddressRoute');
 const commentRouter = require('./src/Route/CommentRoute');
 const apiDocRouter = require('./src/Route/APIDocsRoute');
 const blogRouter = require('./src/Route/BlogRoute');
-const AuthController = require('./src/Controllers/AuthController');
-const jwtHelper = require('./src/middleware/jwtHelper');
-// const mongoString = 'mongodb+srv://admin:doanhnhangroup@cluster0.jqsm5.mongodb.net/user?retryWrites=true&w=majority';
-const mongoString = 'mongodb://nhannt:nhannt1905@13.58.149.178:27017/keep-exploring?authSource=admin&w=1';
+
+const mongoString = 'mongodb://keepExploringUser:keepExploringUser@13.58.149.178:27017/keep-exploring?authSource=keep-exploring&w=1';
 const port = process.env.PORT || 3000;
 const app = express();
 const forms = multer();
@@ -30,31 +28,6 @@ app.use(
 app.use(passport.initialize());
 app.use(cors());
 
-//Routers
-
-// app.post("/login", AuthController.login);
-// app.post("/refresh-token", AuthController.refreshToken);
-// app.post("/logout", AuthController.logout);
-// app.use(jwtHelper.isAuth);
-// app.get("/testAuth", (req, res, next) => {
-//   try {
-//     const friends = [
-//       {
-//         name: "Cat: Russian Blue",
-//       },
-//       {
-//         name: "Cat: Maine Coon",
-//       },
-//       {
-//         name: "Cat: Balinese",
-//       },
-//     ];
-//     return res.status(200).send({ data: friends });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-//
 app.use('/api-doc', apiDocRouter);
 app.use('/user', userRouter);
 app.use('/post', postRouter);
@@ -77,13 +50,11 @@ mongoose
 		console.error(`Error connecting to the database. \n${err}`);
 	});
 
-// Handler 404 error page not found
 app.use((req, res, next) => {
 	const error = new Error('Not found');
 	error.status = 404;
 	next(error);
 });
-// Handler custom error page not found
 app.use((error, req, res, next) => {
 	res.status(error.status || 500);
 	res.send({
