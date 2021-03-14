1cfa8be4fb1c749e3778dfc8cb07ae0a14bd2d19
Trong Nhan <trongnhannguyen94@gmail.com>
2021-03-15 00:36:08 +0700

    init table post page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import LoadingComponent from "common-components/loading/loading";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import PrimeReact from "primereact/api";
 import NotFound from "pages/not-found/not-found";
 import { Suspense } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import LoginPage from "./pages/login-page/login-page";
 import MainPage from "./pages/main-page/main-page";
-
+PrimeReact.ripple = true;
 function App() {
   return (
     <div className="app">
---
+++ src/pages/post-page/components/table-post/table-post.jsx
@@ -0,0 +1,307 @@
+import { Button } from "primereact/button";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { Dialog } from "primereact/dialog";
+import { Rating } from "primereact/rating";
+import { Toast } from "primereact/toast";
+import { Paginator } from 'primereact/paginator';
+import React, { useRef, useState } from "react";
+import "./table-post.scss";
+import postList from "../../post-list.json";
+function TablePostComponent() {
+  console.log(postList);
+  let emptyProduct = {
+    id: null,
+    name: "",
+    image: null,
+    description: "",
+    category: null,
+    price: 0,
+    quantity: 0,
+    rating: 0,
+    inventoryStatus: "INSTOCK",
+  };
+
+  const [products, setProducts] = useState(null);
+  const [productDialog, setProductDialog] = useState(false);
+  const [deleteProductDialog, setDeleteProductDialog] = useState(false);
+  const [deleteProductsDialog, setDeleteProductsDialog] = useState(false);
+  const [product, setProduct] = useState(emptyProduct);
+  const [selectedProducts, setSelectedProducts] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+  const [globalFilter, setGlobalFilter] = useState(null);
+  const toast = useRef(null);
+  const dt = useRef(null);
+
+  const formatCurrency = (value) => {
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  const hideDeleteProductDialog = () => {
+    setDeleteProductDialog(false);
+  };
+
+  const hideDeleteProductsDialog = () => {
+    setDeleteProductsDialog(false);
+  };
+
+  const editProduct = (product) => {
+    setProduct({ ...product });
+    setProductDialog(true);
+  };
+
+  const confirmDeleteProduct = (product) => {
+    setProduct(product);
+    setDeleteProductDialog(true);
+  };
+
+  const deleteProduct = () => {
+    let _products = products.filter((val) => val.id !== product.id);
+    setProduct(_products);
+    setDeleteProductDialog(false);
+    setProduct(emptyProduct);
+    toast.current.show({
+      severity: "success",
+      summary: "Successful",
+      detail: "Product Deleted",
+      life: 3000,
+    });
+  };
+
+  const findIndexById = (id) => {
+    let index = -1;
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  };
+
+  const confirmDeleteSelected = () => {
+    setDeleteProductsDialog(true);
+  };
+
+  const deleteSelectedProducts = () => {
+    let _products = products.filter((val) => !selectedProducts.includes(val));
+    setProducts(_products);
+    setDeleteProductsDialog(false);
+    setSelectedProducts(null);
+    toast.current.show({
+      severity: "success",
+      summary: "Successful",
+      detail: "Products Deleted",
+      life: 3000,
+    });
+  };
+
+  const onCategoryChange = (e) => {
+    let _product = { ...product };
+    _product["category"] = e.value;
+    setProduct(_product);
+  };
+
+  const onInputChange = (e, name) => {
+    const val = (e.target && e.target.value) || "";
+    let _product = { ...product };
+    _product[`${name}`] = val;
+
+    setProduct(_product);
+  };
+
+  const onInputNumberChange = (e, name) => {
+    const val = e.value || 0;
+    let _product = { ...product };
+    _product[`${name}`] = val;
+
+    setProduct(_product);
+  };
+
+  const imageBodyTemplate = (rowData) => {
+    return (
+      <img
+        src={`showcase/demo/images/product/${rowData.image}`}
+        onError={(e) =>
+          (e.target.src =
+            "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
+        }
+        alt={rowData.image}
+        className="product-image"
+      />
+    );
+  };
+
+  const priceBodyTemplate = (rowData) => {
+    return formatCurrency(rowData.price);
+  };
+
+  const ratingBodyTemplate = (rowData) => {
+    return <Rating value={rowData.rating} readOnly cancel={false} />;
+  };
+
+  const statusBodyTemplate = (rowData) => {
+    return (
+      <span className={`product-badge status-${rowData.status.toLowerCase()}`}>
+        {rowData.status}
+      </span>
+    );
+  };
+
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <React.Fragment>
+        <Button
+          icon="pi pi-pencil"
+          className="p-button-rounded p-button-success p-mr-2"
+          onClick={() => editProduct(rowData)}
+        />
+      </React.Fragment>
+    );
+  };
+
+  const header = (
+    <div className="table-header">
+      <h5 className="p-m-0">Manage Products</h5>
+
+      <i
+        className="pi pi-trash"
+        onClick={() => {
+          if (selectedProducts && selectedProducts.length) {
+            confirmDeleteSelected();
+          }
+        }}
+      />
+    </div>
+  );
+
+  const deleteProductDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="No"
+        icon="pi pi-times"
+        className="p-button-text"
+        onClick={hideDeleteProductDialog}
+      />
+      <Button
+        label="Yes"
+        icon="pi pi-check"
+        className="p-button-text"
+        onClick={deleteProduct}
+      />
+    </React.Fragment>
+  );
+  const deleteProductsDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="No"
+        icon="pi pi-times"
+        className="p-button-text"
+        onClick={hideDeleteProductsDialog}
+      />
+      <Button
+        label="Yes"
+        icon="pi pi-check"
+        className="p-button-text"
+        onClick={deleteSelectedProducts}
+      />
+    </React.Fragment>
+  );
+
+  return (
+    <div className="table-post-container">
+      <Toast ref={toast} />
+      <DataTable
+        ref={dt}
+        value={postList}
+        selection={selectedProducts}
+        onSelectionChange={(e) => setSelectedProducts(e.value)}
+        dataKey="id"
+        paginator
+        resizableColumns={true}
+        rows={10}
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport"
+        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+        globalFilter={globalFilter}
+        header={header}
+        scrollable
+        scrollHeight="calc(100% - 200px)"
+        className="p-datatable-gridlines"
+      >
+        <Column
+          selectionMode="multiple"
+          headerStyle={{ width: "3rem" }}
+        ></Column>
+        <Column field="owner" header="User" sortable></Column>
+        <Column field="category" header="Category" sortable></Column>
+        <Column
+          field="status"
+          header="Status"
+          body={statusBodyTemplate}
+          sortable
+        ></Column>
+
+        <Column field="title" header="Title" sortable></Column>
+        <Column field="desc" header="Description" sortable></Column>
+
+        <Column field="address" header="Address" sortable></Column>
+        <Column
+          field="rating"
+          header="Rating"
+          body={ratingBodyTemplate}
+          sortable
+        ></Column>
+        <Column field="created_on" header="Date" sortable></Column>
+        <Column body={actionBodyTemplate} header="Full View"></Column>
+      </DataTable>
+
+      <Dialog
+        visible={deleteProductDialog}
+        style={{ width: "450px" }}
+        header="Confirm"
+        modal
+        footer={deleteProductDialogFooter}
+        onHide={hideDeleteProductDialog}
+      >
+        <div className="confirmation-content">
+          <i
+            className="pi pi-exclamation-triangle p-mr-3"
+            style={{ fontSize: "2rem" }}
+          />
+          {product && (
+            <span>
+              Are you sure you want to delete <b>{product.name}</b>?
+            </span>
+          )}
+        </div>
+      </Dialog>
+      <Dialog
+        visible={deleteProductsDialog}
+        style={{ width: "450px" }}
+        header="Confirm"
+        modal
+        footer={deleteProductsDialogFooter}
+        onHide={hideDeleteProductsDialog}
+      >
+        <div className="confirmation-content">
+          <i
+            className="pi pi-exclamation-triangle p-mr-3"
+            style={{ fontSize: "2rem" }}
+          />
+          {product && (
+            <span>Are you sure you want to delete the selected products?</span>
+          )}
+        </div>
+      </Dialog>
+    </div>
+  
+  
+  
+  );
+}
+
+export default TablePostComponent;
