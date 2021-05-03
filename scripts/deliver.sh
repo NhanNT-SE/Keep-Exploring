967af02c758474b0510592cd1a79073ff22d0352
Nguyễn Trọng Nhân <77843422+NhanNT-SE@users.noreply.github.com>
2021-05-03 13:44:19 +0700

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,5 +1,6 @@
 cd ~/.ssh
 ssh -tt -i web-key.pem ec2-user@ec2-18-191-159-74.us-east-2.compute.amazonaws.com -o StrictHostKeyChecking=no << EOF 
+cd /home/ec2-user/projects/keep-exploring/web-app
 pm2 stop react-app
 pm2 delete react-app
 pm2 serve build 5000 --name "react-app" --spa
