447fbcffafb6f9ff2092315f6918dc76f8dd65e2
NhanNT-SE <nhannt.se1905@gmail.com>
2021-05-28 11:09:36 +0700

    setup ngrx

---
+++ src/app/store/app.state.ts
@@ -0,0 +1,4 @@
+import { AuthState } from './auth/auth.state';
+export interface AppState {
+  feature_auth: AuthState;
+}
---
+++ src/app/store/auth/auth.selector.ts
@@ -0,0 +1,25 @@
+import { AuthState } from './auth.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+export const feature_auth = createFeatureSelector<AuthState>('feature_auth');
+
+export const auth_userListSelector = createSelector(
+  feature_auth,
+  (state) => state.userList
+);
+export const auth_userSelector = createSelector(
+  feature_auth,
+  (state) => state.user
+);
+export const auth_statusSelector = createSelector(
+  feature_auth,
+  (state) => state.status
+);
+export const auth_errorSelector = createSelector(
+  feature_auth,
+  (state) => state.error
+);
+export const auth_sortSelector = createSelector(
+  feature_auth,
+  (state) => state.sort
+);
