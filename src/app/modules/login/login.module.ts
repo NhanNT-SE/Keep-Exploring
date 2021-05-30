7cc6d38f28d340a98b0a08021f56c4500df89488
NhanNT-SE <nhannt.se1905@gmail.com>
2021-05-30 12:59:50 +0700

    develop base layout login

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -6,9 +6,26 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from '../../components/login-component/login.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, CalendarModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    CalendarModule,
+    HttpClientModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    DividerModule,
+    CheckboxModule,
+    ButtonModule,
+  ],
 })
 export class LoginModule {}
